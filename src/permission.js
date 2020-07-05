5e0898bdd01f0f7d500e6248b0bfd9eb0da9009e
bingtengaoyu <992899491@qq.com>
2020-07-05 17:34:06 +0800

    路由跳转添加nprogress

--- src/permission.js
+++ src/permission.js
@@ -1,9 +1,21 @@
 import router from './router'
 
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
+
 router.beforeEach(async(to, from, next) => {
+  NProgress.start()
   // set page title
   // document.title = getPageTitle(to.meta.title)
   // 通过路由钩子，进行权限校验以及标题设置
   next();
+  NProgress.done()
 
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
 })
