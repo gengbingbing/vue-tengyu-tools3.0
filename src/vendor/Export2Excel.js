5c2ef9fc459648e1ee9eb258277ede375cbb18f7
gengbingbing <992899491@qq.com>
2021-05-12 21:42:01 +0800

    修改宽度样式

--- src/vendor/Export2Excel.js
+++ src/vendor/Export2Excel.js
@@ -137,9 +137,9 @@ export function export_json_to_excel(th, jsonData, defaultTitle) {
     }
     /*再判断是否为中文*/
     else if (val.toString().charCodeAt(0) > 255) {
-      return { 'wch': val.toString().length * 2 };
+      return { 'wch': val.toString().length * 2 + 10 };
     } else {
-      return { 'wch': val.toString().length };
+      return { 'wch': val.toString().length * 2 };
     }
   }))
   /*以第一行为初始值*/
