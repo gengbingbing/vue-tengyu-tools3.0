7b002d9865fbbf436e796e0c2d3a4c362824a755
gengbingbing <992899491@qq.com>
2021-06-27 21:23:49 +0800

    test

--- src/__tests__/ex.spec.js
+++ src/__tests__/ex.spec.js
@@ -4,9 +4,11 @@ import index from '@/views/appHead/AppHead.vue';
 
 // eslint-disable-next-line no-undef
 describe('index.vue', () => {
+  // eslint-disable-next-line no-undef
   it('renders text', () => {
     const msg = '管理员';
     const wrapper = shallowMount(index);
+    // eslint-disable-next-line no-undef
     expect(wrapper.text()).toContain(msg);
   });
 });
